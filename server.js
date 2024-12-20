e16252746bf323ae3e36d67ac3572c95eeee3de1
YanNaingSoe243 <boyan.243@gmail.com>
2024-12-20 10:31:53 +0630

    Project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve the Flutter web app
+app.use(express.static(path.join(__dirname, 'web')));
+console.log(__dirname);
+// Handle fallback for single-page apps
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
